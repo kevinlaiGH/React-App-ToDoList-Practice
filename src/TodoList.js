66c644cc0b3194059057c8a804ec1cabab68134a
Kevin Lai <kevin.km.lai@gmail.com>
2017-11-29 16:27:08 +1100

    ToDoItems

---
+++ src/TodoList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class TodoList extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            items: []
+          };
+        this.addItem = this.addItem.bind(this);
+      }
+
+      addItem(e) {
+        var itemArray = this.state.items;
+       
+        if (this._inputElement.value !== "") {
+          itemArray.unshift(
+            {
+              text: this._inputElement.value,
+              key: Date.now()
+            }
+          );
+       
+          this.setState({
+            items: itemArray
+          });
+       
+          this._inputElement.value = "";
+        }
+       
+        console.log(itemArray);
+         
+        e.preventDefault();
+      }
+ 
+   render() {
+   return (
+     <div className="todoListMain">
+       <div className="header">
+       <form onSubmit={this.addItem}>
+           <input ref={(a) => this._inputElement = a} 
+                placeholder="enter task">
+           </input>
+           <button type="submit">add</button>
+         
+        </form>
+       </div>
+     </div>
+   );
+ }
+};
+
+export default TodoList;
